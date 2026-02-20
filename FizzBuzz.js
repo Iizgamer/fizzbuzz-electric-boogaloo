3487c5c83e71cc93ff3cfa1bf49b5f4ce4c1b891
Nicholas Foles _ Student - WakeIBTechEC <npfoles@students.wcpss.net>
2026-02-20 00:04:05 -0500

    finished

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,34 +1,21 @@
 // TODO: Define constants for the form and the result display area
-const numSubmission = document.getElementById('number').value;
+const numSubmission = document.getElementById('number');
 const results = document.getElementById('result');
 const submitButton = document.getElementById('submit');
 
 // TODO: Add the first line of the event listener to handle form submission
-submitButton.addEventListener('submit', handleSubmission());
-
-function handleSubmission() {
-    num = Number(numSubmission)
+submitButton.addEventListener('click', function(Event) {
+    Event.preventDefault();
+    num = Number(numSubmission.value);
     if (num % 3 == 0 && num % 5 == 0) {
-        results.innerHTML = 'FizzBuzz'
+        results.textContent = 'FizzBuzz';
     } else if (num % 3 == 0) {
-        results.innerHTML = 'Fizz'
+        results.textContent = 'Fizz';
     } else if (num % 5 == 0) {
-        results.innerHTML = 'Buzz'
+        results.textContent = 'Buzz';
+    } else if (isNaN(num)) {
+        results.textContent = 'Now why would you do that.'
     } else {
-        results.innerHTML = numSubmission
+        results.textContent = num;
     }
-}
-
-    // Prevent the form from refreshing the page
-    event.preventDefault();
-
-// TODO: Get the number from the form input
-
-// TODO: Write FizzBuzz logic here
-// If divisible by both 3 and 5, set the output to "FizzBuzz"
-// If divisible by 3, set the output to "Fizz"
-// If divisible by 5, set the output to "Buzz"
-// Otherwise, set the output to the number itself
-
-// Display the result on the page
-result.textContent = output;
+});
